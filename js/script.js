55fdd5942be331f8930a573ecc4773df4ce4d9fd
shivangranawat <shivangranawat@itgeeks.com>
2025-10-04 13:30:37 +0530

    done

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let btn = document.getElementById("searching")
+const settingsBtn = document.getElementById('setting');
+const sidePanel = document.getElementById('sidePanel');
+
+
+btn.addEventListener("click", function () {
+    let search = document.getElementById("sarch-bar-pages");
+    search.style.visibility = "visible"
+    search.style.opacity = "1"
+    search.style.transitionDelay = "0s"
+    let dis = document.getElementById("search-bg")
+    dis.style.display = "block"
+})
+
+
+let btn2 = document.getElementById("search-close")
+btn2.addEventListener("click", function () {
+    let search = document.getElementById("sarch-bar-pages");
+    search.style.visibility = "hidden"
+    let dis = document.getElementById("search-bg")
+    dis.style.display = "none"
+})
+
+
+settingsBtn.addEventListener('click', () => {
+    sidePanel.classList.toggle('open');
+});
+
+function closePanel() {
+    sidePanel.classList.remove('open');
+}
